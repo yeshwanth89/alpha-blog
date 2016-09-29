c2ad2d4db82530fecc9ee0dc0a73bc5d73fa6b12
Yeshwanth <yeshwanthkumar.d@gmail.com>
2016-09-29 18:05:17 +0000

    Made changes with debugger

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -18,7 +18,7 @@ class ArticlesController < ApplicationController
 
   def create
     #render plain: params[:article].inspect
-    debugger
+    #debugger
     @article = Article.new(article_params)
     @article.user = current_user
     if @article.save
