7b5a3e9a0a2efdd2f03bf7d384db9ff27af8359d
Yeshwanth <yeshwanthkumar.d@gmail.com>
2016-09-29 17:46:49 +0000

    Did Authentication

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,19 +1,19 @@
 class  PagesController < ApplicationController
-    def homepage
-      
-      
+    def home
+      redirect_to articles_path if logged_in?
+
     end
-  
-  
-  
+
+
+
     def about
-      
-      
-      
-      
+
+
+
+
     end
-  
-  
- 
-  
+
+
+
+
 end
