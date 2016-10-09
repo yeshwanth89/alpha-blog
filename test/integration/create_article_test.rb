f1d345129b2e799dd169dcf25d0fa399d7079385
Yeshwanth <yeshwanthkumar.d@gmail.com>
2016-10-09 07:30:13 +0000

    Added up my tests again

--- test/integration/create_article_test.rb
+++ test/integration/create_article_test.rb
@@ -1,7 +1,26 @@
 require 'test_helper'
 
 class CreateArticleTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
+
+  def setup
+    @user = User.create(username: "yesh", email: "yesh@yesh.com", password: "password")
+  end
+
+
+  test "the article creation form and upon clicking it should go to that particular article" do
   #   assert true
-  # end
+    sign_in_as(@user, 'password')
+
+    get new_article_path
+    assert_template 'articles/new'
+
+    # checks count after test article was made
+    assert_difference 'Article.count', 1 do
+      post_via_redirect articles_path, article: { title: "SAMPLE ARTICLE",
+        description: "this is the integration test for creating articles"}
+      end
+    # redirect and look for title of test article
+    assert_template 'articles/show'
+    assert_match 'SAMPLE ARTICLE', response.body
+  end
 end
