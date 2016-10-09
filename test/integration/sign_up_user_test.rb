0a2549c0657a66a60673a721faa3c96fee88ec46
Yeshwanth <yeshwanthkumar.d@gmail.com>
2016-10-09 07:32:18 +0000

    Added up signup_test to my remote repo

--- test/integration/sign_up_user_test.rb
+++ test/integration/sign_up_user_test.rb
@@ -1,7 +1,15 @@
 require 'test_helper'
 
 class SignUpUserTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
+  test "the  user sign Up, redirect to user page upon being successful" do
   #   assert true
-  # end
+    get signup_path
+    assert_template 'users/new'
+    #hitting the test db and checking if the user is successfully created or not
+     assert_difference 'User.count', 1 do
+      post_via_redirect users_path, user: { username: "yesh", email: "yesh@yesh.com", password: "password"}
+    end
+    #checking if its redirected to the user's page or  not
+    assert_template 'users/show'
+  end
 end
